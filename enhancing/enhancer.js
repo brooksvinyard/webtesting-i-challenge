7adada5511d04f86fb8e6050be1285874cdd916a
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-08 14:45:11 -0700

    Testing MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,3 +1,25 @@
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
+
+item = {
+  'name': 'sweet sword',
+  'enhancement': 10,
+  'durability': 50
+};
+
+item2 = {
+  'name': 'best sword',
+  'enhancement': 20,
+  'durability': 100
+};
+
+item3 = {
+  'name': 'bad sword',
+  'enhancement': 0,
+  'durability': 0
+};
+
 module.exports = {
   succeed,
   fail,
@@ -5,18 +27,51 @@ module.exports = {
   get,
 };
 
+// a success(item) method that accepts an item object and returns a new item object 
+// modified according to the rules defined by the client for enhancement success.
+
+// The item's enhancement increases by 1.
+// If the item enhancement level is 20, the enhancement level is not changed.
+// The durability of the item is not changed.
 function succeed(item) {
+  if (item.enhancement === 20) {
+    item.enhancement = 20;
+  } else {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
 
+// a fail(item) method that accepts an item object and returns a new item object 
+// modified according to the rules defined by the client for enhancement failure.
+
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 
+// (17 goes down to 16, 18 goes down to 17).
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+  } else {
+    item.durability = item.durability - 10;
+  }
+
+  if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1;
+  } 
+
   return { ...item };
 }
 
+// a repair(item) method that accepts an item object and returns a new 
+// item with the durability restored to 100. This method is the simplest 
+// of the three and would be a good starting point on this project.
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
+// a get() method for use when working on the stretch problem.
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,108 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+describe('repair', () => {
+
+    it('should return the durability === 100', () => {
+        expect(enhancer.repair(item)).toEqual({
+            'name': 'sweet sword',
+            'enhancement': 10,
+            'durability': 100
+        });
+    });
+
+    it('should return the durability === 100', () => {
+        expect(enhancer.repair(item2)).toEqual({
+            'name': 'best sword',
+            'enhancement': 20,
+            'durability': 100
+        });
+    });
+
+    it('should return the durability === 100', () => {
+        expect(enhancer.repair(item3)).toEqual({
+            'name': 'bad sword',
+            'enhancement': 0,
+            'durability': 100
+        });
+    });
+});
+
+describe('succeed', () => {
+
+    it('should return the enhancement +1', () => {
+        expect(enhancer.succeed({
+            'name': 'sweet sword',
+            'enhancement': 10,
+            'durability': 50
+          })).toEqual({
+            'name': 'sweet sword',
+            'enhancement': 11,
+            'durability': 50
+        });
+    });
+
+    it('should return the enhancement +1', () => {
+        expect(enhancer.succeed({
+            'name': 'best sword',
+            'enhancement': 20,
+            'durability': 100
+          })).toEqual({
+            'name': 'best sword',
+            'enhancement': 20,
+            'durability': 100
+        });
+    });
+
+    it('should return the enhancement +1', () => {
+        expect(enhancer.succeed({
+            'name': 'bad sword',
+            'enhancement': 0,
+            'durability': 0
+          })).toEqual({
+            'name': 'bad sword',
+            'enhancement': 1,
+            'durability': 0
+        });
+    });
+});
+
+describe('fail', () => {
+
+    it('should return the durability -5', () => {
+        expect(enhancer.fail({
+            'name': 'sweet sword',
+            'enhancement': 10,
+            'durability': 50
+          })).toEqual({
+            'name': 'sweet sword',
+            'enhancement': 10,
+            'durability': 45
+        });
+    });
+
+    it('should return the durability -10', () => {
+        expect(enhancer.fail({
+            'name': 'medium sword',
+            'enhancement': 15,
+            'durability': 50
+          })).toEqual({
+            'name': 'medium sword',
+            'enhancement': 15,
+            'durability': 40
+        });
+    });
+
+    it('should return the enhancement -1 and durability -10', () => {
+        expect(enhancer.fail({
+            'name': 'best sword',
+            'enhancement': 20,
+            'durability': 100
+          })).toEqual({
+            'name': 'best sword',
+            'enhancement': 19,
+            'durability': 90
+        });
+    });
+});
